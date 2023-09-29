9a53a5e56e90a202ea29ac04df9b4b310c051403
gwpcastro <146481202+gwpcastro@users.noreply.github.com>
2023-09-29 15:51:14 -0300

    Update pessoa.py

--- Classes_Base/pessoa.py
+++ Classes_Base/pessoa.py
@@ -1,1 +1,6 @@
 # Definicao da classe compra, que armazena os dados de uma pessoa
+class Pessoa:
+  def __init__(self, nome:str , email:str, cpf:str):
+    self.nome = nome
+    self.email = email
+    self.cpf = cpf
