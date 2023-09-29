362b99173031ae2efd7ddca4213f022d9ad03342
gwpcastro <146481202+gwpcastro@users.noreply.github.com>
2023-09-29 15:57:59 -0300

    Update produto.py

--- Classes_Base/produto.py
+++ Classes_Base/produto.py
@@ -1,1 +1,9 @@
 # Definicao da classe compra, que representa um produto da loja
+class Produto:
+  def __init__(self,nome:str, preco:float, qtdestoque:float, descpercent:float, categoria:str):
+    self.nome = nome
+    self.preco = preco
+    self.__qtdestoque = 0
+    self.__descpercent = 0
+    self.categoria = categoria
+    
